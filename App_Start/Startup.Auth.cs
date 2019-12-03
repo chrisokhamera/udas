ae0b5f3136f3875fd83bb0de453204ba469bb0bd
chrisokhamera <chrisokhamera@CHRISOKHAMEF526>
2019-12-03 13:03:24 +0000

    The project has began

---
+++ App_Start/Startup.Auth.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace DoctorPatient
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Enable the application to use a cookie to store information for the signed in user
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+            // Use a cookie to temporarily store information about a user logging in with a third party login provider
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            // Uncomment the following lines to enable logging in with third party login providers
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //   consumerKey: "",
+            //   consumerSecret: "");
+
+            app.UseFacebookAuthentication(
+              appId: "472495810289745",
+              appSecret: "2dda606b4d5292a0131bfb7faeafba94");
+
+            //app.UseGoogleAuthentication(
+             //    clientId: "000-000.apps.googleusercontent.com",
+              //   clientSecret: "00000000000");
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Builder;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoctorPatient.Startup))]
+namespace DoctorPatient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
